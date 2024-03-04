5c8f7faf654a7fd5ef75842baae156294cf112eb
Gee-Wey Yue <geewey@gmail.com>
2024-03-03 23:24:55 -0800

    change rule for newly added URLs

--- background.js
+++ background.js
@@ -58,7 +58,10 @@ function updateRules() {
       id: index + 1,
       priority: 1,
       action: { type: "block" },
-      condition: { urlFilter: `*://${site}/*`, resourceTypes: ["main_frame"] },
+      condition: {
+        urlFilter: `https://${site}*`,
+        resourceTypes: ["main_frame"],
+      },
     }));
 
     chrome.declarativeNetRequest.updateDynamicRules({
